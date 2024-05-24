5aff015b33f163b14c6aba08a2039c5eee1b3cd8
WandersonFeitosa <wanderson.feitosa211@gmail.com>
2024-05-24 12:21:39 -0300

    feat: update minecraft_backup script

--- minecraft_backup.sh
+++ minecraft_backup.sh
@@ -1,25 +1,33 @@
-channelId="1231021187577155644"
-route="https://b5c1b50f-3001.brs.devtunnels.ms/v1/bob/send-server-message"
-remaining_seconds=180
-timeout=60
+#!/bin/bash
+channelId="1242165928804749505"
+apiUrl="http://34.122.46.191"
+msgroute="$apiUrl/v1/bob/send-server-message"
+updateStatusRoute="$apiUrl/v1/minecraft/update-status"
+remaining_seconds=300
 
 function send_message (){ 
-    curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"$1\", \"channelId\": \"$channelId\"}" "$route"
+    curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"$1\", \"channelId\": \"$channelId\"}" "$msgroute"
     screen -S tcsmp -X stuff "say $1^M"
 }
 
-for ((i=0; i < remaining_seconds; i+=timeout)); do
-    remaining_minutes=$((remaining_seconds / 60))
-    remaining_time=$((remaining_minutes > 0 ? remaining_minutes : remaining_seconds))
-    time_label=$([ "$remaining_minutes" -gt 1 ] && echo "minutos" || echo "segundos")
-    message="O servidor será reiniciado para a realização do backup em $remaining_time $time_label, aguarde que logo voltaremos"
-    send_message "$message"
-    sleep $timeout
-    remaining_seconds=$((remaining_seconds - timeout))
+while [ $remaining_seconds -gt 0 ]; do
+    if [ $remaining_seconds -gt 60 ]; then    
+        send_message "O servidor será reiniciado para a realização do backup em $((remaining_seconds / 60)) minutos, aguarde que logo voltaremos"
+        remaining_seconds=$((remaining_seconds - 60))
+        sleep 60
+    elif [ $remaining_seconds -gt 30 ]; then
+        send_message "O servidor será reiniciado para a realização do backup em 1 minuto, aguarde que logo voltaremos"
+        remaining_seconds=$((remaining_seconds - 30))
+        sleep 30
+    else
+        send_message "O servidor será reiniciado para a realização do backup em $remaining_seconds segundos, aguarde que logo voltaremos"
+        sleep 10
+        remaining_seconds=$((remaining_seconds - 10))
+    fi
 done
 
 
-curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"Iniciando backup, servidor fechado\", \"channelId\": \"${channelId}\"}" "$route"
+curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"Iniciando backup, servidor fechado\", \"channelId\": \"${channelId}\"}" "$msgroute"
 screen -S tcsmp -X stuff "stop^M"
 
 sleep 20
@@ -28,10 +36,12 @@ backup_date=$(date +%Y-%m-%d)
 
 zip -r "/home/hd/backup-$backup_date.zip" "/home/ssd/tcsmp"
 
-curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"Backup finalizado, iniciando o servidor\", \"channelId\": \"${channelId}\"}" "$route"
+curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"Backup finalizado, iniciando o servidor\", \"channelId\": \"${channelId}\"}" "$msgroute"
 
 screen -S tcsmp -X stuff "bash run.sh^M"
 
-sleep 60
+rm /home/ssd/tcsmp/backup-lock
 
-curl -X POST -H "Content-Type: application/json" -d "{\"message\": \"Servidor Iniciado\", \"channelId\": \"${channelId}\"}" "$route"
+curl -X PATCH -H "Content-Type: application/json" -d "{\"status\": \"pending\"}" "$updateStatusRoute"
+
+
--- src/controllers/LogsController.ts
+++ src/controllers/LogsController.ts
@@ -1,28 +1,35 @@
 import { Request, Response } from "express";
 import { getFileNamesInFolder } from "../utils/getFileNamesInFolder";
-import * as fs from 'fs';
-
+import * as fs from "fs";
 
 export class LogsController {
-    async getFileNames(req: Request, res: Response) {
-        const fileNames = await getFileNamesInFolder("/home/ssd/ncsmp/logs");
-        res.status(200).json({ fileNames });
-    }
+  logsFilePath = process.env.LOGS_FILE_PATH || "/home/ssd/tcsmp/logs";
+  async getFileNames(req: Request, res: Response) {
+    const fileNames = await getFileNamesInFolder(this.logsFilePath);
+    res.status(200).json({ fileNames });
+  }
 
-    async sendLog(req: Request, res: Response) {
-        const { log } = req.params;
-        const logFilePath = `/home/ssd/ncsmp/logs/${log}`;
+  async sendLog(req: Request, res: Response) {
+    const { log } = req.params;
+    const logFilePath = `${this.logsFilePath}/${log}`;
 
-        try {
-            fs.readFile(logFilePath, (err, data) => {
-                if (err) {
-                    res.status(404).json({ message: "Arquivo não encontrado, por favor verifique se o nome está correto", sucess: false });
-                } else {
-                    res.send(data);
-                }
-            });
-        } catch (err) {
-            res.status(404).json({ message: "Arquivo não , por favor verifique se o nome está correto", sucess: false });
+    try {
+      fs.readFile(logFilePath, (err, data) => {
+        if (err) {
+          res.status(404).json({
+            message:
+              "Arquivo não encontrado, por favor verifique se o nome está correto",
+            sucess: false,
+          });
+        } else {
+          res.send(data);
         }
+      });
+    } catch (err) {
+      res.status(404).json({
+        message: "Arquivo não , por favor verifique se o nome está correto",
+        sucess: false,
+      });
     }
+  }
 }
